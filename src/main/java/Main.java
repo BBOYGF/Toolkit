d88ce831857539ab414959d69b7b83b0ad00e7d5
fan <664130988@qq.com>
2022-12-09 00:10:33 +0800

    项目初始化

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 主程序
+ *
+ * @Author guofan
+ * @Create 2022/12/8
+ */
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("测试");
+    }
+}
